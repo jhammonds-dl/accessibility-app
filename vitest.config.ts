36876976c6024b77045e4dde00d73ca1180c42c1
jhammonds-dl <j.hammonds@wearedoubleline.com>
2024-11-25 09:41:28 -0500

    add gh-pages

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 // Configure auto-import of globals like 'it', 'expect', 'describe', etc.
 export default defineConfig({
+    base: "/accessibility-app/",
     test: {
         globals: true,
         coverage: {
